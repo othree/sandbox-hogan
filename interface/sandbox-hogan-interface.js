a7e41adaaf447e944c0e5abfb3950f11038b614c
othree_kao <othree_kao@htc.com>
2013-07-15 16:52:59 +0800

    Improve interface implementation

--- interface/sandbox-hogan-interface.js
+++ interface/sandbox-hogan-interface.js
@@ -1,27 +1,83 @@
-/*jslint vars: true, browser: true */
-/*global chrome: false, Q: false, QX: false, flickr: false*/
+/*jslint vars: true*/
 /**
  * @file An example interface implementation.
  */
 
 
-(function () {
+(function (root) {
     "use strict";
 
+    var Hogan = null;
+
+    var callbacks = {};
+
     var handler = {
-        compileDone: function () {
-            console.log('template ready');
+        compileDone: function (data) {
+            if (callbacks[data.id]) {
+                callbacks[data.id](data);
+                delete callbacks[data.id];
+            }
+        },
+        compileFail: function (data) {
+            if (callbacks[data.id]) {
+                delete callbacks[data.id];
+            }
+            throw new Error('Hogan compile failed in sandbox');
         },
         renderDone: function (data) {
-            document.getElementById('share_txt').innerHTML = data.result;
-        }
+            if (callbacks[data.id]) {
+                callbacks[data.id](data);
+                delete callbacks[data.id];
+            }
+        },
+        renderFail: function (data) {
+            if (callbacks[data.id]) {
+                delete callbacks[data.id];
+            }
+            throw new Error('Hogan render failed in sandbox');
+        },
     };
-
-    window.addEventListener('message', function (event) {
+    var onmessage = function (event) {
         var data = event.data.hogan;
         if (!data) { return; }
         var hand = handler[data.event];
         if (typeof hand === 'function') { hand(data.value); }
-    }, false);
+    };
 
-}());
+    var uid = function () {
+        return ++uid.value;
+    };
+    uid.value = 0;
+
+    var name = function () {
+        return 'SHogan_' + uid();
+    };
+
+    root.SHogan = {
+        set: function (input) {
+            if (Hogan) { Hogan.removeEventListener('message', onmessage); }
+            Hogan = input;
+            Hogan.addEventListener('message', onmessage, false);
+        },
+        compile: function (tpl, callback) {
+            if (!Hogan) { throw new Error('Sandbox Hogan not set'); }
+            var n = name();
+            var id = uid();
+            if (typeof callback === 'function') {
+                callbacks[id] = callback;
+            }
+            Hogan.postMessage({hogan: {compile: [tpl, n, id]}});
+            return n;
+        },
+        render: function (n, data, callback) {
+            if (!Hogan) { throw new Error('Sandbox Hogan not set'); }
+            var id = uid();
+            if (typeof callback === 'function') {
+                callbacks[id] = callback;
+            }
+            Hogan.postMessage({hogan: {render: [data, n, id]}});
+            return;
+        }
+    };
+
+}(this));
